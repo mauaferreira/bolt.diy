a78593e9c6b0d4a36661ca81bacbe5a9f012dbe3
root <root@srv668396.hstgr.cloud>
2025-07-07 20:59:41 +0000

    Permite domínio do EasyPanel em allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Outras configs do seu projeto...
   server: {
     allowedHosts: [
-      "site-boltdiy.ikhu8w.easypanel.host"
+      'site-boltdiy.ikhu8w.easypanel.host'
     ]
   }
-}
+});
